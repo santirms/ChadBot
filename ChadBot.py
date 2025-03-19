cdecd6083874ab09b915c02714a3cfc0e4b88d6c
santirms <santiago.r89@gmail.com>
2025-03-19 17:11:09 -0300

    Update ChadBot.py

--- ChadBot.py
+++ ChadBot.py
@@ -139,5 +139,5 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Render
-    app.run(host="0.0.0.0", port=port=10000)  # Escucha en todas las interfaces
+    app.run(host="0.0.0.0", port=10000)  # Escucha en todas las interfaces
 
