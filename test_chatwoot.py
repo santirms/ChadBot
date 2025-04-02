da7393d66aa11d8a800549c6dc9214e991a32d52
santirms <santiago.r89@gmail.com>
2025-04-02 15:13:53 -0300

    Update test_chatwoot.py

--- test_chatwoot.py
+++ test_chatwoot.py
@@ -3,7 +3,6 @@ import requests
 
 CHATWOOT_URL = os.environ.get("CHATWOOT_URL")  # Ej: https://chat.igeneration.com.ar
 API_KEY = os.environ.get("CHATWOOT_API_KEY")
-
 print(f"🔐 TOKEN RAW: {repr(API_KEY)}")
 
 if not CHATWOOT_URL or not API_KEY:
