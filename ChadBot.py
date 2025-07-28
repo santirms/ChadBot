d962bb799932258a64433a7056fc31b905d81f52
santirms <santiago.r89@gmail.com>
2025-07-27 23:39:24 -0300

    Update ChadBot.py

--- ChadBot.py
+++ ChadBot.py
@@ -202,7 +202,4 @@ def test_chatwoot():
     except Exception as e:
         return f"❌ Excepción: {str(e)}", 500    
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
+app.run(host="0.0.0.0", port=10000)
